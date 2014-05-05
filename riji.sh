43326fa64f9e67d69bf1a75c42c163ee2e6b6095
karupanerura <karupa@cpan.org>
2014-05-05 12:22:57 +0900

    use `riji server` with grunt-connect-proxy

--- riji.sh
+++ riji.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-git add --all .
-git commit -m '*** TEMPORARY COMMIT *** FOR RIJI PUBLISH'
-# and manualy `git rebase -i` and fixup and rewrite comment
 cd `dirname $0`/src/riji
 exec riji $@
