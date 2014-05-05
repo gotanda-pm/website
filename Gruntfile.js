bcbfed8a740b28ea84252e1cde6c1d9da4e52081
karupanerura <karupa@cpan.org>
2014-05-05 12:22:57 +0900

    use `riji server` with grunt-connect-proxy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,16 @@
+var gruntConnectProxyUtils = require('grunt-connect-proxy/lib/utils');
+var exec = require('child_process').exec;
+
 module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
+  grunt.registerTask('riji', 'Start a riji server.', function() {
+    exec('./riji.sh server');
+  });
+
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
-    clean: ["htdocs/css/"],
-    shell: {
-      riji: {
-        command: './riji.sh publish'
-      }
-    },
+    clean: ["htdocs/css/", "htdocs/js/"],
     compass: {
       production: {
         options: {
@@ -44,17 +46,29 @@ module.exports = function (grunt) {
         options: {
           livereload: true,
           base: ['htdocs'],
-          port: 5000
+          port: 5000,
+          middleware: function (connect, options) {
+            var middlewares = options.base.map(function (base) { return connect.static(base); });
+            middlewares.unshift(gruntConnectProxyUtils.proxyRequest);
+            return middlewares;
+          }
         }
-      }
+      },
+      proxies: [
+        {
+          context: [
+            "/",
+            "!/css/",
+            "!/js/"
+          ],
+          host: "localhost",
+          port: 3650
+        }
+      ]
     },
     watch: {
       options: {
         livereload: true
-      },
-      riji: {
-        files: ['src/riji/article/**/*.md', 'src/riji/share/tmpl/**/*.tx'],
-        tasks: ['shell:riji']
       },
       scss: {
         files: ['src/scss/**/*.scss'],
@@ -67,12 +81,11 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask("html",    ["shell:riji"]);
   grunt.registerTask("css",     ["compass:production"]);
   grunt.registerTask("js",      ["uglify:js"]);
-  grunt.registerTask("build",   ["html", "css", "js"]);
+  grunt.registerTask("build",   ["css", "js"]);
 
-  grunt.registerTask("server",  ["connect:app", "watch"]);
+  grunt.registerTask("server",  ["configureProxies", "connect:app", "riji", "watch"]);
   grunt.registerTask("default", ["server"]);
 
   grunt.task.run('notify_hooks');
