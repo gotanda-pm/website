7cd91fc16b68a0f68e449d42d98feeb9b91a1890
mizuki_r <ry.mizuki@gmail.com>
2014-06-23 20:18:32 +0900

    menuボタン撤去

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,6 @@
 "use strict";
 
 var EventBinder = {
-  menuOpener: function (menu, opener) {
-    var $menu = $(menu);
-
-    $(opener).on("click", function () {
-      if ($menu.hasClass("show-menu")) {
-        $menu.removeClass("show-menu");
-      }
-      else {
-        $menu.addClass("show-menu");
-      }
-    });
-
-    var width = $(window).width();
-    setInterval(function () {
-      var current = $(window).width();
-      if (current !== width) {
-        width = current;
-        if (width > 640) {
-          $menu.removeClass("show-menu");
-        }
-      }
-    }, 500);
-  },
   goTop: function (selector) {
     $(selector).on("click", function () {
       $(document.body).animate({ scrollTop: 0 }, 300);
@@ -37,7 +14,6 @@ var EventBinder = {
 };
 
 $(function () {
-  EventBinder.menuOpener(".main-menu", ".open-menu");
   EventBinder.goTop(".btn-gotop");
   EventBinder.pseudoHover("a, input");
 });
